b6366c8b0e63992b77eda8bb1ebea411d0dd84a0
agga1 <agud100@gmail.com>
2021-09-13 15:16:09 +0200

    added test for correctness

--- test/membrane_framerate_converter_test.exs
+++ test/membrane_framerate_converter_test.exs
@@ -13,14 +13,14 @@ defmodule Membrane.FramerateConverterTest do
   alias Membrane.File.{Sink, Source}
   require Membrane.Logger
 
-  @testsrc Path.expand("fixtures/testsrc.h264", __DIR__)
+  @testsrc Path.expand("fixtures/testsrc_2s.h264", __DIR__)
 
   defp expand_path(file_name) do
     Path.expand("fixtures/#{file_name}", __DIR__)
   end
 
-  defp prepare_output() do
-    output_path = expand_path("output.h264")
+  defp prepare_output(file_name) do
+    output_path = expand_path(file_name)
 
     File.rm(output_path)
     on_exit(fn -> File.rm(output_path) end)
@@ -28,9 +28,15 @@ defmodule Membrane.FramerateConverterTest do
     output_path
   end
 
+  defp assert_files_equal(file_a, file_b) do
+    assert {:ok, a} = File.read(file_a)
+    assert {:ok, b} = File.read(file_b)
+    assert a == b
+  end
+
   describe "framreate converter should convert" do
-    defp perform_test(elements, links) do
-      pipeline_options = %Pipeline.Options{elements: elements, links: links}
+    defp perform_test(elements) do
+      pipeline_options = %Pipeline.Options{elements: elements}
       assert {:ok, pid} = Pipeline.start_link(pipeline_options)
 
       assert Pipeline.play(pid) == :ok
@@ -39,27 +45,34 @@ defmodule Membrane.FramerateConverterTest do
     end
 
     test "video with pts" do
-      output_path = prepare_output()
+      output_path = prepare_output("out.h264")
 
       elements = [
         file: %Source{chunk_size: 40_960, location: @testsrc},
-        parser: %Parser{framerate: {10, 1}},
+        parser: %Parser{framerate: {5, 1}},
         decoder: Decoder,
         converter: %Membrane.FramerateConverter{framerate: {2, 1}},
         encoder: Encoder,
         sink: %Sink{location: output_path}
       ]
 
-      links = [
-        link(:file)
-        |> to(:parser)
-        |> to(:decoder)
-        |> to(:converter)
-        |> to(:encoder)
-        |> to(:sink)
+      perform_test(elements)
+    end
+
+    test "video correctly" do
+      output_path = prepare_output("out.yuv")
+      reference_file = expand_path("reference-testsrc_2s.yuv")
+
+      elements = [
+        file: %Source{chunk_size: 40_960, location: @testsrc},
+        parser: %Parser{framerate: {5, 1}},
+        decoder: Decoder,
+        converter: %Membrane.FramerateConverter{framerate: {5, 1}},
+        sink: %Sink{location: output_path}
       ]
 
-      perform_test(elements, links)
+      perform_test(elements)
+      assert_files_equal(output_path, reference_file)
     end
   end
 end
