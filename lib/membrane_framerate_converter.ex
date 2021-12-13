74e333d3ca0fa67869a3436e5e22238a3701a4a3
Agnieszka Dutka <agnieszka.dutka@swmansion.com>
2021-12-13 15:09:17 +0100

    use pts in buffers

--- lib/membrane_framerate_converter.ex
+++ lib/membrane_framerate_converter.ex
@@ -3,13 +3,17 @@ defmodule Membrane.FramerateConverter do
   Element converts video to target constant frame rate, by dropping and duplicating frames as necessary.
   Input video may have constant or variable frame rate.
   Element expects each frame to be received in separate buffer.
-  Additionally, presentation timestamps must be passed in each buffer's metadata.
+  Additionally, presentation timestamps must be passed in each buffer's `pts` fields.
+
+  It is worth noting that the Framerate Converter output video duration may differ slightly from the original duration.
+  In particular, it will not generate timestamps bigger than the last timestamp in the input video -
+  e.g 2 seconds video in 5 fps (having 10 frames total) after conversion to 15 fps will have 28 frames
+  - as the 1.9(3)s and 1.8(6)s timestamps exceed last timestamp of the input video.
   """
 
   use Bunch
   use Membrane.Filter
 
-  alias Membrane.Buffer
   alias Membrane.Caps.Video.Raw
 
   require Membrane.Logger
@@ -35,9 +39,9 @@ defmodule Membrane.FramerateConverter do
       options
       |> Map.from_struct()
       |> Map.merge(%{
-        last_payload: nil,
-        last_metadata: nil,
-        target_pts: 0
+        last_buffer: nil,
+        target_pts: 0,
+        exact_target_pts: 0
       })
 
     {:ok, state}
@@ -48,28 +52,22 @@ defmodule Membrane.FramerateConverter do
     {{:ok, demand: {:input, size}}, state}
   end
 
-  def handle_process(:input, %{metadata: metadata}, _ctx, _state)
-      when not is_map_key(metadata, :pts) do
-    raise("cannot adjust framerate in stream without pts")
-  end
-
   @impl true
   def handle_process(
         :input,
-        %{payload: payload, metadata: metadata} = buffer,
+        buffer,
         _ctx,
-        %{last_payload: nil} = state
+        %{last_buffer: nil} = state
       ) do
-    state =
-      %{state | target_pts: metadata.pts, last_metadata: metadata, last_payload: payload}
-      |> bump_target_pts()
+    state = put_first_buffer(buffer, state)
+    state = bump_target_pts(state)
 
     {{:ok, buffer: {:output, buffer}, redemand: :output}, state}
   end
 
   @impl true
-  def handle_process(:input, %{payload: payload, metadata: metadata}, _ctx, state) do
-    {buffers, state} = create_new_frames(metadata, payload, state)
+  def handle_process(:input, buffer, _ctx, state) do
+    {buffers, state} = create_new_frames(buffer, state)
     {{:ok, [buffer: {:output, buffers}, redemand: :output]}, state}
   end
 
@@ -78,42 +76,44 @@ defmodule Membrane.FramerateConverter do
     {{:ok, caps: {:output, %{caps | framerate: framerate}}}, state}
   end
 
-  defp bump_target_pts(%{target_pts: pts, framerate: {num, denom}} = state) do
-    use Ratio
-    target_pts = pts + Ratio.new(denom * Membrane.Time.second(), num)
-    %{state | target_pts: target_pts}
+  defp put_first_buffer(first_buffer, state) do
+    %{
+      state
+      | target_pts: first_buffer.pts,
+        exact_target_pts: first_buffer.pts,
+        last_buffer: first_buffer
+    }
   end
 
-  defp create_new_frames(metadata, payload, state, buffers \\ []) do
+  defp bump_target_pts(%{exact_target_pts: exact_pts, framerate: {num, denom}} = state) do
+    use Ratio
+    next_exact_pts = exact_pts + Ratio.new(denom * Membrane.Time.second(), num)
+    next_target_pts = Ratio.floor(next_exact_pts)
+    %{state | target_pts: next_target_pts, exact_target_pts: next_exact_pts}
+  end
+
+  defp create_new_frames(input_buffer, state, buffers \\ []) do
     use Ratio
 
-    if Ratio.gt?(state.target_pts, metadata.pts) do
-      state = %{
-        state
-        | last_metadata: metadata,
-          last_payload: payload
-      }
+    if Ratio.gt?(state.target_pts, input_buffer.pts) do
+      state = %{state | last_buffer: input_buffer}
 
       {Enum.reverse(buffers), state}
     else
-      dist_right = metadata.pts - state.target_pts
-      dist_left = state.target_pts - state.last_metadata.pts
+      last_buffer = state.last_buffer
 
-      buffer =
+      dist_right = input_buffer.pts - state.target_pts
+      dist_left = state.target_pts - last_buffer.pts
+
+      new_buffer =
         if Ratio.lte?(dist_left, dist_right) do
-          %Buffer{
-            payload: state.last_payload,
-            metadata: %{state.last_metadata | pts: state.target_pts}
-          }
+          %{last_buffer | pts: state.target_pts}
         else
-          %Buffer{
-            payload: payload,
-            metadata: %{metadata | pts: state.target_pts}
-          }
+          %{input_buffer | pts: state.target_pts}
         end
 
       state = bump_target_pts(state)
-      create_new_frames(metadata, payload, state, [buffer | buffers])
+      create_new_frames(input_buffer, state, [new_buffer | buffers])
     end
   end
 end
