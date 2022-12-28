26365ab7675c43575a476c632898d75c58cad21c
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-12-28 16:12:01 +0100

    Dump version 0.5.1 ~> 0.6.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.FramerateConverter.Mixfile do
   use Mix.Project
 
-  @version "0.5.1"
+  @version "0.6.0"
   @github_url "https://github.com/membraneframework/membrane_framerate_converter_plugin"
 
   def project do
